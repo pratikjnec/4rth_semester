c096dc2ded43fe4770cfb169b92effe0a8abd5cc
pratikjnec <pratikjnec2005@gmail.com>
2025-01-15 13:07:28 +0530

    selection sort function implemented

--- DAA/practical_2/claculate_time_taken.c
+++ DAA/practical_2/claculate_time_taken.c
@@ -41,7 +41,27 @@ clock_t insertionSort(int arr[], int n)
     return time_required = clock();
 
 } 
+
+void selectionSort(int arr[], int n) 
+{ 
+    int i, j, min_idx; 
   
+    for (i = 0; i < n-1; i++) 
+    { 
+        min_idx = i; 
+        for (j = i+1; j < n; j++) 
+        if (arr[j] < arr[min_idx]) 
+            min_idx = j; 
+  
+        int temp = arr[min_idx]; 
+        arr[min_idx] = arr[i]; 
+        arr[i] = temp; 
+    }
+
+    return time_required = clock();
+
+} 
+
 void printArray(int arr[], int n) 
 { 
     int i;
