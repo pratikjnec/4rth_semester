f27ed7e1ece648ff6fb0cc04c1b8e2f7875c8b1c
pratikjnec <pratikjnec2005@gmail.com>
2025-01-15 13:14:52 +0530

    corrected code implemented

--- DAA/practical_2/claculate_time_taken.c
+++ DAA/practical_2/claculate_time_taken.c
@@ -1,100 +1,94 @@
 #include <stdio.h>
-#include<time.h>
-clock_t time_required = 0;
-clock_t time_req;
+#include <time.h>
 
-clock_t bubbleSort(int arr[], int n) 
-{ 
-    int i, j; 
-    for (i = 0; i < n-1; i++) 
-    { 
-        for (j = 0; j < n-i-1; j++)  
-        { 
-            if (arr[j] > arr[j+1]) 
-            { 
-                int temp = arr[j]; 
-                arr[j] = arr[j+1]; 
-                arr[j+1] = temp; 
-            } 
-        } 
-    } 
-    
-    return time_required = clock();
-
-} 
-
-clock_t insertionSort(int arr[], int n) 
-{ 
-    int i, key, j; 
-    for (i = 1; i < n; i++) 
-    { 
-        key = arr[i]; 
-        j = i - 1; 
-  
-        while (j >= 0 && arr[j] > key) 
-        { 
-            arr[j + 1] = arr[j]; 
-            j = j - 1; 
-        } 
-        arr[j + 1] = key; 
-    } 
-    return time_required = clock();
-
-} 
-
-void selectionSort(int arr[], int n) 
-{ 
-    int i, j, min_idx; 
-  
-    for (i = 0; i < n-1; i++) 
-    { 
-        min_idx = i; 
-        for (j = i+1; j < n; j++) 
-        if (arr[j] < arr[min_idx]) 
-            min_idx = j; 
-  
-        int temp = arr[min_idx]; 
-        arr[min_idx] = arr[i]; 
-        arr[i] = temp; 
+void bubbleSort(int arr[], int n) {
+    int i, j;
+    for (i = 0; i < n - 1; i++) {
+        for (j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
     }
+}
 
-    return time_required = clock();
+void insertionSort(int arr[], int n) {
+    int i, key, j;
+    for (i = 1; i < n; i++) {
+        key = arr[i];
+        j = i - 1;
 
-} 
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+}
 
-void printArray(int arr[], int n) 
-{ 
-    int i;
-    printf("Sorted array: \n"); 
-    for (i = 0; i < n; i++) 
-        printf("%d ", arr[i]); 
-    printf("\n");  
-} 
-  
-int main() 
-{ 
-    int arr[] = { 12, 11, 13, 5, 6 }; 
-    time_req = clock();
-    int n = sizeof(arr) / sizeof(arr[0]);
+void selectionSort(int arr[], int n) {
+    int i, j, min_idx;
+    for (i = 0; i < n - 1; i++) {
+        min_idx = i;
+        for (j = i + 1; j < n; j++) {
+            if (arr[j] < arr[min_idx])
+                min_idx = j;
+        }
+        int temp = arr[min_idx];
+        arr[min_idx] = arr[i];
+        arr[i] = temp;
+    }
+}
 
-    printf("Unsorted array: \n"); 
-    for (int i = 0; i < n; i++) 
-        printf("%d ", arr[i]); 
-    printf("\n");  
-  
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
 
-    printf("\n\nInsertion Sort\n"); 
-    time_required = insertionSort(arr, n);
-    printArray(arr, n); 
-    printf("Time Required for Bubble sort algorithm: %lf",  ((float)time_required / CLOCKS_PER_SEC));
+void copyArray(int original[], int copy[], int n) {
+    for (int i = 0; i < n; i++)
+        copy[i] = original[i];
+}
 
+int main() {
+    int original[] = {12, 11, 13, 5, 6};
+    int n = sizeof(original) / sizeof(original[0]);
+    int* arr = malloc(n * sizeof(int));
+    
+    printf("Unsorted array:\n");
+    printArray(original, n);
 
-    printf("\n\nBubble Sort\n"); 
-    time_required = bubbleSort(arr, n);
+    clock_t start, end;
+
+    // Insertion Sort
+    copyArray(original, arr, n);
+    start = clock();
+    insertionSort(arr, n);
+    end = clock();
+    printf("\nInsertion Sort:\n");
     printArray(arr, n);
-    printf("Time Required for Bubble sort algorithm: %lf", ((float)time_required / CLOCKS_PER_SEC));
+    printf("Time Required: %lf seconds\n", (double)(end - start) / CLOCKS_PER_SEC);
 
-    time_required = clock();
-    printf("\n\nTime Required for insertion sort algorithm: %lf", time_required);
-    return 0; 
+    // Bubble Sort
+    copyArray(original, arr, n);
+    start = clock();
+    bubbleSort(arr, n);
+    end = clock();
+    printf("\nBubble Sort:\n");
+    printArray(arr, n);
+    printf("Time Required: %lf seconds\n", (double)(end - start) / CLOCKS_PER_SEC);
+
+    // Selection Sort
+    copyArray(original, arr, n);
+    start = clock();
+    selectionSort(arr, n);
+    end = clock();
+    printf("\nSelection Sort:\n");
+    printArray(arr, n);
+    printf("Time Required: %lf seconds\n", (double)(end - start) / CLOCKS_PER_SEC);
+
+    return 0;
 }
